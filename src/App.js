422a92b846b600dc3d25d8e33afb7e0838ec5f7e
lbcamargo94 <lb.camargo94@gmail.com>
2022-01-13 18:13:15 -0300

    [Grupo 25] requisito 1 - criado formulario de login :white_check_marck:

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from 'react';
 import logo from './trivia.png';
+import Routes from './Routes/Routes';
 import './App.css';
 
 export default function App() {
   return (
     <main className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+      <Routes />
     </main>
   );
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,43 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div />
+    <form action="">
+      {/* INPUT EMAIL */}
+      <div>
+        <label
+          htmlFor="inputEmaill"
+        >
+          Email address
+          <input
+            data-testid="input-gravatar-email"
+            id="inputEmaill"
+            placeholder="Enter your email"
+            type="email"
+          />
+        </label>
+      </div>
+      {/* INOPUT NickName */}
+      <div>
+        <label 
+          htmlFor="playerName"
+        >
+          NickName
+          <input
+            type="text"
+            id="playerName"
+            data-testid="input-player-name"
+            placeholder="Enter your player name"
+          />
+        </label>
+      </div>
+      {/* BTN PLAY */}
+      <div>
+        <button 
+          data-testid="btn-play"
+        >
+          Play
+        </button>
+      </div>
+    </form>
   );
 }
