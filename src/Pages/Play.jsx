a5ed155972491ec7cd330c24290b10c7fe0e4fe2
Luis Gustavo <l.gustavomelo@hotmail.com>
2022-01-14 18:21:45 -0300

    get quiz

--- src/Pages/Play.jsx
+++ src/Pages/Play.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import getQuestions from '../services/getQuestions';
 
 export default function Play() {
+  const [quiz, setQuiz] = useState('');
+  const { token } = useSelector((state) => state);
+
+  useEffect(() => {
+    const getQuiz = async () => {
+      const result = await getQuestions(token);
+      // console.log(result);
+      setQuiz(result);
+    };
+    getQuiz();
+  }, [token]);
+
   return (
     <section>
-      <h1>Jogo</h1>
+      {/* Categoria */}
+      <div>
+        <h1 data-testid="question-category">category</h1>
+      </div>
+      {/* Perguntas que vem da API */}
+      <div>
+        <h3 data-testid="question-text">perguntas</h3>
+      </div>
+      {/* Alternativas da API */}
+      <div>
+        <ul>
+          <li>
+            teste
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
